229f2ce96e5779cdae8deebb7fff2aded09ae25c
sukistore <cockids105@gmail.com>
2026-06-07 16:22:53 +0700

    Enhance service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
+const CACHE_NAME = 'suki-store-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
